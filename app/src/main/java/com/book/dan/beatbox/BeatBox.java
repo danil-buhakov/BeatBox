824861dd567d722cdabb21b04864f18e04be28f0
Danil Buhakov <tradinal@gmai.com>
2018-01-26 16:02:57 +0200

    Added data binding and sound assets

---
+++ app/src/main/java/com/book/dan/beatbox/BeatBox.java
@@ -0,0 +1,27 @@
+package com.book.dan.beatbox;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+import android.util.Log;
+
+import java.io.IOException;
+
+public class BeatBox {
+    private static final String TAG = "BeatBox";
+    private static final String SOUND_FOLDER = "sample_sounds";
+    private AssetManager mAssetManager;
+    public BeatBox(Context context){
+        mAssetManager = context.getAssets();
+        loadSamples();
+    }
+    private void loadSamples(){
+        String[] soundNames;
+        try {
+            soundNames = mAssetManager.list(SOUND_FOLDER);
+            Log.i(TAG,"Found "+soundNames.length+" sounds");
+        } catch (IOException e) {
+            Log.e(TAG,"Could not list assets", e);
+            return;
+        }
+    }
+}
--- app/src/main/java/com/book/dan/beatbox/BeatBoxFragment.java
+++ app/src/main/java/com/book/dan/beatbox/BeatBoxFragment.java
@@ -1,7 +1,69 @@
 package com.book.dan.beatbox;
 
-public class BeatBoxFragment {
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.book.dan.beatbox.databinding.FragmentBeatBoxBinding;
+import com.book.dan.beatbox.databinding.ListItemSoundBinding;
+
+public class BeatBoxFragment extends Fragment {
+    private BeatBox mBeatBox;
     public static BeatBoxFragment newInstance(){
         return new BeatBoxFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        FragmentBeatBoxBinding binding = DataBindingUtil.inflate(inflater, R.layout.fragment_beat_box,
+                container,false);
+
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(getActivity(),3));
+        binding.recyclerView.setAdapter(new SoundAdapter());
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        mBeatBox = new BeatBox(getActivity());
+    }
+
+    private class SoundHolder extends RecyclerView.ViewHolder{
+        private ListItemSoundBinding mBinding;
+
+        public SoundHolder(ListItemSoundBinding binding) {
+            super(binding.getRoot());
+            mBinding = binding;
+        }
+    }
+
+    private class SoundAdapter extends RecyclerView.Adapter<SoundHolder>{
+
+        @Override
+        public SoundHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+            ListItemSoundBinding binding = DataBindingUtil
+                    .inflate(inflater,R.layout.list_item_sound,parent,false);
+            return new SoundHolder(binding);
+        }
+
+        @Override
+        public void onBindViewHolder(SoundHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 }
