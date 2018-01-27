c1515e9a92b005becaa96f1f9d03bfb18ab9fe2f
Danil Buhakov <tradinal@gmai.com>
2018-01-27 15:42:09 +0200

    Added data binding variables and connected views with models

---
+++ app/src/main/java/com/book/dan/beatbox/SoundViewModel.java
@@ -0,0 +1,25 @@
+package com.book.dan.beatbox;
+
+import android.databinding.BaseObservable;
+
+public class SoundViewModel extends BaseObservable {
+    private Sound mSound;
+    private BeatBox mBeatBox;
+
+    public SoundViewModel(BeatBox beatBox){
+        mBeatBox = beatBox;
+    }
+
+    public String getTitle(){
+        return mSound.getName();
+    }
+
+    public Sound getSound() {
+        return mSound;
+    }
+
+    public void setSound(Sound sound) {
+        mSound = sound;
+        notifyChange();
+    }
+}
